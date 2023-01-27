1d81dbd045f881f5a01a23ef0edfcc4916e1cf68
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2023-01-27 11:47:26 -0500

    Fix for anonymous authors

--- ao3-personal-bookmarking.js
+++ ao3-personal-bookmarking.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            AO3 Personal Bookmarking
 // @namespace       https://github.com/MonoScyron/ao3-scripts
-// @version         0.0.1
+// @version         0.0.2
 // @description     Personal bookmark formatting script, activates on pressing kudos button.
 // @author          MonoScyron
 // @updateURL       https://raw.githubusercontent.com/MonoScyron/ao3-scripts/main/ao3-personal-bookmarking.js
@@ -16,7 +16,7 @@
     let inputBookmark = function() {
         let fandom = document.querySelector('dl.work.meta.group dd.fandom.tags a[href]').innerText;
         let title = document.querySelector("#workskin div.preface.group h2.title.heading").innerText;
-        let author = document.querySelector("#workskin div.preface.group h3.byline.heading a[rel='author']").innerText;
+        let author = document.querySelector("#workskin div.preface.group h3.byline.heading").innerText;
 
         // Open bookmarks editor & input formatted notes
         document.getElementsByClassName("bookmark_form_placement_open")[0].click();
