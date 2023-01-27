453643891008863fe6a06c932ca7bb47b5cdd488
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2023-01-27 13:47:15 -0500

    Display list of personal bookmark tags

--- ao3-personal-bookmarking.js
+++ ao3-personal-bookmarking.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            AO3 Personal Bookmarking
 // @namespace       https://github.com/MonoScyron/ao3-scripts
-// @version         0.0.2
+// @version         0.0.3
 // @description     Personal bookmark formatting script, activates on pressing kudos button.
 // @author          MonoScyron
 // @updateURL       https://raw.githubusercontent.com/MonoScyron/ao3-scripts/main/ao3-personal-bookmarking.js
@@ -18,10 +18,25 @@
         let title = document.querySelector("#workskin div.preface.group h2.title.heading").innerText;
         let author = document.querySelector("#workskin div.preface.group h3.byline.heading").innerText;
 
-        // Open bookmarks editor & input formatted notes
+        // Open bookmarks editor
         document.getElementsByClassName("bookmark_form_placement_open")[0].click();
+
+        // Input formatted notes
         let bookmarkInput = document.querySelector("fieldset #bookmark_notes");
         bookmarkInput.value = `${title} by ${author} [${fandom}]\n\nsum`;
+
+        // Display personal bookmark tags under bookmark input
+        const bookmarkTags = [" read for fluff", " read for pain", " read for big brain takes",
+            " read for character", " read for spicy", " rotating...", " oof"];
+        let bookmarkTagDisplayHTML = `<span class=" LV_validation_message LV_invalid">
+            ${bookmarkTags}
+        </span>`;
+
+        let bookmarkTagDisplay = document.createElement('div');
+        bookmarkTagDisplay.innerHTML = bookmarkTagDisplayHTML;
+        bookmarkTagDisplay = bookmarkTagDisplay.firstChild;
+
+        document.querySelector("#bookmark-form ul.autocomplete").appendChild(bookmarkTagDisplay);
     }
 
     // If fic is not currently bookmarked
