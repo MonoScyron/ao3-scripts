972a4fa1a18d015d9aea974aa78cc38205b8bbf6
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2023-02-09 16:49:04 -0500

    Finish comments dropdown

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Simple script that calculates the ratio between a work's kudos and hits and adds
 
 Personal bookmark formatting script, activates on pressing bookmark button.
 
-## AO3 Comments Dropdown (WIP)
+## AO3 Comments Dropdown
 
 Adds a dropdown button for randomly generated nice commments.
--- src/ao3-comments-dropdown.ts
+++ src/ao3-comments-dropdown.ts
@@ -3,5 +3,25 @@
     let tracery = require('tracery-grammar');
     let grammar = tracery.createGrammar(JSON.parse(GM_getResourceText("cJSON")));
     grammar.addModifiers(tracery.baseEngModifiers);
-    console.log(grammar.flatten("ao3ex: #origin#"));
+
+    const DROPDOWN_HTML = `
+    <p class="footnote" style="margin-bottom: -15px;">
+        <select name="nice-comment" id="nice-comment-dropdown">
+            <option selected="selected" value="clear"></option>
+            <option value="re_read">Re-read</option>
+            <option value="love">Love</option>
+            <option value="angst">Crying</option>
+        </select>
+    </p>
+    `;
+    let dropdownWrap = document.createElement('p');
+    dropdownWrap.innerHTML = DROPDOWN_HTML;
+    dropdownWrap = dropdownWrap.firstElementChild as HTMLParagraphElement;
+    let dropdown = dropdownWrap.firstElementChild as HTMLSelectElement;
+
+    const COMMENT_INPUT = document.querySelector("textarea[id*='comment_content_for']") as HTMLTextAreaElement;
+    dropdown.addEventListener('change', () => COMMENT_INPUT.value = grammar.flatten(`#${dropdown.value}#`));
+
+    let comment = document.querySelector("form.new_comment fieldset")!;
+    comment.insertBefore(dropdownWrap, comment.children[3]);
 })();
